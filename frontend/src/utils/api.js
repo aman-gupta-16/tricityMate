6ef26e5dd1bc2040f248603c897abea0fb459b15
Aman gupta <159292186+aman-gupta-16@users.noreply.github.com>
2025-01-06 15:27:18 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", // your backend API base URL
+  baseURL: "https://tricitymate-backend-g6gc.onrender.com/api", // your backend API base URL
 });
 
 // Axios interceptor to add token to headers
